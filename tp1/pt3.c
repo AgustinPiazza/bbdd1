1a743cc9dddebe339d9a2022886455737a1f0396
agusspzz <aguspiazza2003@gmail.com>
2024-08-24 16:08:38 -0300

    c

--- tp1/pt3.c
+++ tp1/pt3.c
@@ -41,6 +41,8 @@ struct estRegistro *definicionRegistro()
     struct estRegistro *r = (struct estRegistro *)malloc(sizeof(struct estRegistro));
     int cantidad, longitud;
     printf("Ingrese la cantidad de campos que quiere añadir:    \n");
+    // c. De alguna forma, el usuario le indicará al programa cuando haterminado de definir la estructura de la lista de clientes
+    // No se a que se refieren con eso, si para eso les preguntamos cuantos campos van a ser...
     scanf("%d", &cantidad);
     struct campo *lista = (struct campo *)malloc(sizeof(struct campo) * cantidad);
     // VALIDAR QUE CANTIDAD SEA MAYOR A 0;
@@ -58,8 +60,40 @@ struct estRegistro *definicionRegistro()
     return r;
 }
 
-char *ingresoRegistro(struct estRegistro *){
 
+
+//Mas o menos lo que vi, es hacer un ciclo con la cantidad de campos (no se si hay que escribirla en el archivo o directamente usarla en el ciclo)
+//Ir escribiendo el el archivo cada nombre con su longitud, hasta que termine el ciclo
+
+void guardarStructEnArchivo(archivo, struct){
+
+    fwrite(r->cantidad)
+
+    for (int i = 0; i < r->cantidad; i++){
+        fwrite(campos_nombre, tamaño, archivo donde lo guardas);
+        fwrite(campos_longitud, "", "");
+    }
+    
+
+}
+
+//calculamos el tamaño del registro primero, creamos el registro y despues con otro ciclo vamos cargando los datos de cada campo 
+//despues escribir el registro ya cargado en el archivo.
+
+
+char *ingresoRegistro(struct estRegistro *r){
+
+    int tamañoRegistro;
+    for (int i = 0; i < r->cantidad; i++){
+        /* code */
+    }
+
+    char *registro = (char *)malloc(tamañoRegistro);
+    
+    //carga de datos
+
+    FILE *archivo = fopen;
+    fwrite(registro, sizeof(char), tamañoRegistro, archivo)
 }
 
 void limpiarBuffer()
